27642d880b01d0331a340da23f59c6b20878b200
Riot <riotcse@gmail.com>
2017-10-31 18:18:09 +0530

    Update bootnode

--- boot-node-docker/bootnode.sh
+++ boot-node-docker/bootnode.sh
@@ -1,4 +1,4 @@
 IP_ADDRESS=$(wget -qO- http://ipecho.net/plain; echo)
 
-geth init /root/genesis.json --datadir '$HOME/.ethereum/sentinel' && \
-geth --identity '$IP_ADDRESS:bootnode' --datadir '$HOME/.ethereum/sentinel' --networkid 14781492
+geth init /root/genesis.json --datadir "$HOME/.ethereum/sentinel" && \
+geth --identity "$IP_ADDRESS:bootnode" --datadir "$HOME/.ethereum/sentinel" --networkid 87411478

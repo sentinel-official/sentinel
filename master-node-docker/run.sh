52793d7d91bc9ca226e42b4a475f6ab345983153
Tony Stark <ironman0x7b2@protonmail.com>
2017-12-13 12:18:26 +0530

    Updated master node and VPN node
    Former-commit-id: 38f5c46fffe24b26362e60b0963a198c85e39900

--- master-node-docker/run.sh
+++ master-node-docker/run.sh
@@ -1,13 +1,14 @@
 IP_ADDRESS=$(wget -qO- http://ipecho.net/plain; echo)
+SENTINEL_DIR=$HOME/.ethereum/sentinel;
 
 cd /root;
 
-if [ "$SENT_ENV" != "dev" ]; then
-  geth init genesis.json --datadir "$HOME/.ethereum/sentinel" && \
-  nohup geth --identity "$IP_ADDRESS:masternode" --datadir "$HOME/.ethereum/sentinel" --networkid 87411478 >> /dev/null &
+if [ "$SENT_ENV" != "DEV" ]; then
+  geth init genesis.json --datadir "$SENTINEL_DIR" && \
+  nohup geth --identity "$IP_ADDRESS:master-node" --datadir "$SENTINEL_DIR" --networkid 87411478 >> /dev/null &
 else
-  geth init genesis.json --datadir "$HOME/.ethereum/sentinel" && \
-  nohup geth --identity "$IP_ADDRESS:masternode" --datadir "$HOME/.ethereum/sentinel" --networkid 87411478 \
+  geth init genesis.json --datadir "$SENTINEL_DIR" && \
+  nohup geth --identity "$IP_ADDRESS:master-node" --datadir "$SENTINEL_DIR" --networkid 87411478 \
     --fast --rpc --rpcaddr=0.0.0.0 --rpccorsdomain "*" --rpcapi="admin,debug,eth,miner,net,personal,rpc,txpool,web3" \
     --nodiscover --mine --minerthreads 1 >> /dev/null &
 fi
@@ -15,4 +16,4 @@ fi
 sleep 5;
 
 nohup mongod >> /dev/null &
-gunicorn --reload -b 0.0.0.0:8000 app:app
+gunicorn --reload -b 0.0.0.0:8000 app:app;

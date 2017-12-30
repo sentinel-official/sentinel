f9cd75e9fb87aab9e98a09f35753d734513360bf
Tony Stark <ironman0x7b2@protonmail.com>
2017-12-30 12:11:44 +0530

    Added Sentinel coin smart contract.

---
+++ smart-contracts/coin_only/Owned.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.19;
+
+contract Owned {
+	address public owner;
+
+	function Owned() public {
+		owner = msg.sender;
+	}
+
+	modifier onlyOwner {
+		require(msg.sender == owner);
+		_;
+	}
+
+	function transferOwnership(address _owner) onlyOwner public {
+		owner = _owner;
+	}
+}

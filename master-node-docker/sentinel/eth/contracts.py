93a4cd2ba12ae867a09ed9aae3020d28388d6731
Tony Stark <ironman0x7b2@protonmail.com>
2018-01-03 14:29:55 +0530

    Updated ap

--- master-node-docker/sentinel/eth/contracts.py
+++ master-node-docker/sentinel/eth/contracts.py
@@ -1,25 +1,31 @@
 from .eth import eth_manager
-from ..config import CONTRACT_ABI
-from ..config import CONTRACT_ADDRESS
-from ..config import CONTRACT_NAME
+from ..config import SENTINEL_ABI
+from ..config import SENTINEL_ADDRESS
+from ..config import SENTINEL_NAME
+
+from ..config import VPNSERVICE_ABI
+from ..config import VPNSERVICE_ADDRESS
+from ..config import VPNSERVICE_NAME
+
 from ..config import COINBASE_PASSWORD
 from ..config import DECIMALS
 
 
 class ContractManager(object):
-    def __init__(self, name, abi, address, password):
-        self.abi = abi
-        self.name = name
-        self.address = address
+    def __init__(self, sentinel, vpn_service, password):
+        self.sentinel = sentinel
+        self.vpn_service = vpn_service
         self.eth_manager = eth_manager
         self.coinbase = eth_manager.web3.eth.coinbase
         self.password = password
-        self.contract = self.eth_manager.web3.eth.contract(
-            contract_name=name, abi=abi, address=address)
+        self.contracts = [
+            self.eth_manager.web3.eth.contract(contract_name=sentinel['name'], abi=sentinel['abi'], address=sentinel['address']),
+            self.eth_manager.web3.eth.contract(contract_name=vpn_service['name'], abi=vpn_service['abi'], address=vpn_service['address'])
+        ]
 
     def get_balance(self, account_addr):
         try:
-            balance = self.contract.call(
+            balance = self.contracts[0].call(
                 {'from': account_addr}).balanceOf(account_addr)
         except Exception as err:
             return {'code': 201, 'error': str(err)}, None
@@ -31,11 +37,11 @@ class ContractManager(object):
             self.eth_manager.web3.personal.unlockAccount(
                 account_addr, password)
             if session_id is None:
-                tx_hash = self.contract.transact(
+                tx_hash = self.contracts[0].transact(
                     {'from': account_addr}).transfer(to_addr, amount)
             else:
-                tx_hash = self.contract.transact(
-                    {'from': account_addr}).payVpnSession(amount, session_id)
+                tx_hash = self.contracts[1].transact(
+                    {'from': account_addr}).payVpnSession(sentinel['address'], amount, session_id)
             self.eth_manager.web3.personal.lockAccount(account_addr)
         except Exception as err:
             return {'code': 202, 'error': str(err)}, None
@@ -43,23 +49,23 @@ class ContractManager(object):
 
     def get_due_amount(self, account_addr):
         try:
-            due = self.contract.call({'from': account_addr}).getDueAmount()
+            due = self.contracts[1].call({'from': account_addr}).getDueAmountOf(account_addr)
         except Exception as err:
             return {'code': 203, 'error': str(err)}, None
         return None, due
 
     def get_vpn_sessions(self, account_addr):
         try:
-            sessions = self.contract.call(
-                {'from': account_addr}).getVpnSessions()
+            sessions = self.contracts[1].call(
+                {'from': account_addr}).getVpnSessionsOf(account_addr)
         except Exception as err:
             return {'code': 204, 'error': str(err)}, None
         return None, sessions
 
     def get_vpn_usage(self, account_addr, index):
         try:
-            usage = self.contract.call(
-                {'from': account_addr}).getVpnUsage(index)
+            usage = self.contracts[1].call(
+                {'from': account_addr}).getVpnUsageOf(account_addr, index)
         except Exception as err:
             return {'code': 205, 'error': str(err)}, None
         return None, usage
@@ -69,9 +75,9 @@ class ContractManager(object):
         try:
             self.eth_manager.web3.personal.unlockAccount(
                 account_addr, password)
-            tx_hash = self.contract.transact(
+            tx_hash = self.contracts[1].transact(
                 {'from': account_addr}).addVpnUsage(
-                    to_addr, received_bytes, sent_bytes, session_duration,
+                    to_addr, received_bytes, session_duration,
                     amount, timestamp)
             self.eth_manager.web3.personal.lockAccount(account_addr)
         except Exception as err:
@@ -80,4 +86,6 @@ class ContractManager(object):
 
 
 contract_manager = ContractManager(
-    CONTRACT_NAME, CONTRACT_ABI, CONTRACT_ADDRESS, COINBASE_PASSWORD)
+    {'name': SENTINEL_NAME, 'abi': SENTINEL_ABI, 'address': SENTINEL_ADDRESS},
+    {'name': VPNSERVICE_NAME, 'abi': VPNSERVICE_ABI, 'address': VPNSERVICE_ADDRESS},
+    COINBASE_PASSWORD)

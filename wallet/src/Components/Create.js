e8781f7cbf12f492469e82808320e159eecdc016
fabulous345 <fabulous345@protonmail.com>
2018-01-10 12:37:31 +0530

    done create auid screen
    Former-commit-id: 89e4b5bfa0a5bfafc05824a2e363b39252f91454

--- wallet/src/Components/Create.js
+++ wallet/src/Components/Create.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { Toolbar, ToolbarGroup, TextField, RaisedButton, Chip, Dialog, FlatButton, Checkbox } from 'material-ui';
+import { Toolbar, ToolbarGroup, TextField, RaisedButton, Chip, Dialog, FlatButton, Checkbox, Paper } from 'material-ui';
 import Dashboard from './Dashboard';
-import { createAccount,uploadKeystore } from '../Actions/AccountActions';
+import { createAccount, uploadKeystore } from '../Actions/AccountActions';
 
 class Create extends Component {
     constructor(props) {
@@ -16,13 +16,12 @@ class Create extends Component {
             account_addr: '',
             private_key: '',
             keystore_addr: '',
-            checked:false
+            checked: false
         }
         this.set = this.props.set;
     }
 
     onChange = (event) => {
-        event.persist();
         var input = event.target;
         var reader = new FileReader();
         reader.onload = () => {
@@ -35,7 +34,7 @@ class Create extends Component {
 
     _createAccount = () => {
         var password = this.state.password;
-        var that=this;
+        var that = this;
         createAccount(password, function (err, account) {
             if (err) console.log(err, "Error");
             else {
@@ -49,12 +48,12 @@ class Create extends Component {
         });
     }
 
-    _store=()=>{
-        var keystore=this.state.keystore;
-        var that=this;
-        uploadKeystore(keystore,function(err){
+    _store = () => {
+        var keystore = this.state.keystore;
+        var that = this;
+        uploadKeystore(keystore, function (err) {
             if (err) console.log(err);
-            else{
+            else {
                 that.set('dashboard');
             }
         })
@@ -66,58 +65,46 @@ class Create extends Component {
                 primary={true}
                 disabled={!this.state.checked}
                 onClick={() => { this.set('dashboard') }}
-                labelStyle={{textTransform:'capitalize'}}
-                
+                labelStyle={{ textTransform: 'capitalize' }}
+
             />,
         ];
         return (
             <MuiThemeProvider>
                 <div>
-                    <Toolbar style={{ backgroundColor: '#223366' }}>
+                    <Toolbar style={{ backgroundColor: 'rgb(83, 45, 145)' }}>
                         <ToolbarGroup>
-                            <center><p style={{ color: 'white' }}>Sign Up</p></center>
+                            <img src={'../src/Images/2.png'} style={{ height: 50, width: 50 }} />
+                            <p style={styles.toolbarTitle}>SENTINEL-ANON PLATFORM</p>
                         </ToolbarGroup>
                     </Toolbar>
-                    <Grid>
-                        <Row>
-                            <Col xs={3}></Col>
-                            <Col xs={8}>
-                                <TextField
-                                    floatingLabelText="Password"
-                                    type="password"
-                                    onChange={(password) => { this.setState({ password }) }}
-                                />
-                                <RaisedButton label="Create Wallet" primary={true}
-                                    disabled={this.state.password === '' ? true : false}
-                                    onClick={this._createAccount} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={5} />
-                            <Col md={2}>
-                                <p>(OR)</p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={3}>
-                                <div>
-                                    <input type="file" style={{ display: 'none' }} id="filepicker"
-                                        onChange={this.onChange.bind(this)} />
-                                </div>
-                            </Col>
-                            <Col md={4}>
-                                <RaisedButton label="Upload Keystore" primary={true} style={{ width: '80%' }}
+                    <div style={{ marginLeft: '7%', marginRight: '7%' }}>
+                        <h3 style={{ color: 'rgb(83, 45, 145)', fontSize: 14, marginBottom: 0, marginTop: '7%' }}>Create your AUID</h3>
+                        <hr width="50%" align="left" size="3" noshade style={{ backgroundColor: 'rgb(83, 45, 145)' }} />
+                        <Paper zDepth={2} style={{ height: 35, width: '100%', backgroundColor: 'rgba(229, 229, 229, 0.66)', marginTop: '5%' }}>
+                            <TextField
+                                hintText="Enter Password to create AUID"
+                                hintStyle={{ fontSize: 12, color: 'rgba(0, 0, 0, 0.66)' }}
+                                type="password"
+                                onChange={(password) => { this.setState({ password }) }}
+                                style={{ width: '85%', paddingLeft: '5%', height: 40 }}
+                            />
+                        </Paper>
+                        <RaisedButton label="Create"
+                            labelStyle={{ color: 'white', textTransform: 'none' }}
+                            disabled={this.state.password === '' ? true : false}
+                            onClick={this._createAccount}
+                            buttonStyle={{ backgroundColor: 'rgba(83, 45, 145, 0.71)', height: '30px', lineHeight: '30px' }}
+                            style={{ marginTop: '5%', marginBottom: '3%' }} />
+                        <p style={{ fontSize: 12, marginLeft: '3%' }}>(Or)</p>
+                        <Paper zDepth={2} style={{ backgroundColor: 'rgba(181, 216, 232, 0.32)', marginTop: '7%' }}>
+                            <div style={{ padding: '7%' }}>
+                                <RaisedButton
+                                    label="Select keystore file"
+                                    labelStyle={{ color: 'white', textTransform: 'none' }}
                                     onClick={() => { document.getElementById('filepicker').click() }}
+                                    buttonStyle={{ backgroundColor: 'rgba(128, 128, 128, 0.66)', height: '30px', lineHeight: '30px' }}
                                     disabled={this.state.file === '' ? false : true} />
-                            </Col>
-                            <Col md={4}>
-                                <RaisedButton label="Send" disabled={this.state.file === '' ? true : false}
-                                    onClick={this._store.bind(this)} />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={3} />
-                            <Col md={3}>
                                 {this.state.file == '' ?
                                     <div></div>
                                     :
@@ -125,32 +112,68 @@ class Create extends Component {
                                         {this.state.file}
                                     </Chip>
                                 }
-                            </Col>
-                            <Dialog
-                                actions={actions}
-                                modal={true}
-                                open={this.state.openDialog}
-                            >
-                                <b>Account Address:</b><p style={{fontSize:'12px'}}>{this.state.account_addr}</p>
-                                <b>Private Key:</b><p style={{fontSize:'12px'}}>{this.state.private_key}</p>
-                                <b>Keystore stored at:</b><p style={{fontSize:'12px'}}>{this.state.keystore_addr}</p>
-                                <Checkbox
-                                label="I have saved my Private Key securely"
-                                checked={this.state.checked}
-                                onCheck={()=>{this.setState((oldState) => {
+                                <Paper zDepth={2} style={{ height: 35, width: '100%', marginTop: '3%' }}>
+                                    <TextField
+                                        hintText="Enter keystore password"
+                                        hintStyle={{ fontSize: 12, color: 'rgba(0, 0, 0, 0.45)' }}
+                                        type="password"
+                                        style={{ width: '85%', paddingLeft: '5%', height: 40 }}
+                                    />
+                                </Paper>
+                                <RaisedButton
+                                    label="Restore"
+                                    labelStyle={{ color: 'white', textTransform: 'none' }}
+                                    disabled={this.state.file === '' ? true : false}
+                                    onClick={this._store.bind(this)}
+                                    buttonStyle={{ backgroundColor: 'rgba(83, 45, 145, 0.71)', height: '30px', lineHeight: '30px' }}
+                                    style={{ marginTop: '5%' }} />
+                                <input type="file" style={{ display: 'none' }} id="filepicker"
+                                    onChange={this.onChange.bind(this)} />
+                            </div>
+                        </Paper>
+                    </div>
+                    <Toolbar style={{ backgroundColor: 'rgb(83, 45, 145)', bottom: 0 }}>
+                        <ToolbarGroup>
+                            <Row>
+                                <Col xsOffset={4} xs={4}>
+                                    <p>Sentinel@2018</p>
+                                </Col>
+                            </Row>
+                        </ToolbarGroup>
+                    </Toolbar>
+                    <Dialog
+                        actions={actions}
+                        modal={true}
+                        open={this.state.openDialog}
+                    >
+                        <b>Account Address:</b><p style={{ fontSize: '12px' }}>{this.state.account_addr}</p>
+                        <b>Private Key:</b><p style={{ fontSize: '12px' }}>{this.state.private_key}</p>
+                        <b>Keystore stored at:</b><p style={{ fontSize: '12px' }}>{this.state.keystore_addr}</p>
+                        <Checkbox
+                            label="I have saved my Private Key securely"
+                            checked={this.state.checked}
+                            onCheck={() => {
+                                this.setState((oldState) => {
                                     return {
-                                      checked: !oldState.checked,
+                                        checked: !oldState.checked,
                                     };
-                                  });
-                              }}
-                                />
-                            </Dialog>
-                        </Row>
-                    </Grid>
+                                });
+                            }}
+                        />
+                    </Dialog>
                 </div>
             </MuiThemeProvider>
         );
     }
+}
+
+const styles = {
+    toolbarTitle: {
+        color: 'white',
+        marginLeft: 10,
+        fontSize: 14,
+        fontWeight: '600'
+    },
 }
 
 export default Create;

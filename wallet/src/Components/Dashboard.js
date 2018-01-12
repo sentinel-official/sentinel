349ba6ce5e1a4524326c6e324a6de1aa1a30743f
fabulous345 <fabulous345@protonmail.com>
2018-01-12 18:35:30 +0530

    Added VPN in  Header
    Former-commit-id: 1986f8cfd063573725e488eddddf6caf73739808

--- wallet/src/Components/Dashboard.js
+++ wallet/src/Components/Dashboard.js
@@ -1,109 +1,126 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { Toolbar, ToolbarGroup, ToolbarSeparator ,TextField, RaisedButton, Chip, Tabs, Tab } from 'material-ui';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, TextField, RaisedButton, Chip, Tabs, Tab } from 'material-ui';
 import SendComponent from './SendComponent';
 import tab from 'material-ui/svg-icons/action/tab';
 import Header from './Header';
-import { getBalance, getAccount, transferAmount} from '../Actions/AccountActions';
+import { getBalance, getAccount, transferAmount, getVPNdetails } from '../Actions/AccountActions';
 import History from './History';
 import ReceiveComponent from './ReceiveComponent';
+import VPNComponent from './VPNComponent';
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 'send',
-            password: '',
-            activeTab: 'purple',
-            color: 'purple',
-            local_address: '',
-            balance: {
-              sents:0,
-              eths:0
-            },
-            isGetBalanceCalled: false
-        }
-        this.set = this.props.set;
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'send',
+      password: '',
+      activeTab: 'purple',
+      color: 'purple',
+      local_address: '',
+      balance: {
+        sents: 'Loading',
+        eths: 'Loading'
+      },
+      isGetBalanceCalled: false,
+      vpnData: null,
+      status: false
     }
-    componentWillMount() {
-      let that = this;
-      
-      getAccount( (err, account_addr) => {
-        if(err) console.log(err)
-        else {
-          that.setState({
-            local_address: account_addr
-          })
-        }
-      } );
-    }
+    this.set = this.props.set;
+  }
+  componentWillMount() {
+    let that = this;
 
-    getUserBalance() {
-      let balanceEth = {
-        account_addr: this.state.local_address,
-        unit: 'ETH'
+    getAccount((err, account_addr) => {
+      if (err) console.log(err)
+      else {
+        that.setState({
+          local_address: account_addr
+        })
       }
-      let that = this;
-      getBalance( balanceEth , (err, balance) => {
-        if(err) console.log(err, 'got and error')
-        else {
-          that.setState({ balance })
-        }
-      } )
+    });
+  }
 
+  getUserBalance() {
+    let balanceEth = {
+      account_addr: this.state.local_address,
+      unit: 'ETH'
     }
-    handleChange = (value) => {
-      this.setState({
-        value: value,
-        color: 'orange'
-      });
-    };
-
-    render() {
-      let that = this;
-      if (!this.state.isGetBalanceCalled) {
-        setInterval(function () {
-  
-          that.getUserBalance();
-        }, 5000);
-        
-        this.setState({isGetBalanceCalled: true});
+    let that = this;
+    getBalance(balanceEth, (err, balance) => {
+      if (err) console.log(err, 'got and error')
+      else {
+        that.setState({ balance })
       }
+    })
+  }
 
-        return (
-            <MuiThemeProvider>
-                <div>
-                  <div>
-                  <div>
-                    <Header balance={this.state.balance}  local_address={this.state.local_address} />
+  getVPNapi = () => {
+    var that = this;
+    getVPNdetails(function (status, data) {
+      that.setState({ status: status, vpnData: data });
+    })
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+      color: 'orange'
+    });
+  };
+
+  render() {
+    let that = this;
+    if (!this.state.isGetBalanceCalled) {
+      setInterval(function () {
+
+        that.getUserBalance();
+      }, 5000);
+
+      this.setState({ isGetBalanceCalled: true });
+    }
+
+    return (
+      <MuiThemeProvider>
+        <div>
+          <div>
+            <div>
+              <Header balance={this.state.balance} onChange={this.getVPNapi} local_address={this.state.local_address} />
+              <div>
+                <Tabs
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  tabItemContainerStyle={{
+                    backgroundColor: '#FAFAFA'
+                  }}
+                  inkBarStyle={{ backgroundColor: '#532d91' }}
+                >
+                  <Tab style={{ fontSize: 14, fontWeight: 'bold', color: '#532d91' }} label="HISTORY" value="history">
+                    <History local_address={this.state.local_address} />
+                  </Tab>
+                  <Tab style={{ fontSize: 14, fontWeight: 'bold', color: '#532d91' }} label="SEND" value="send">
+                    <SendComponent local_address={this.state.local_address} />
+                  </Tab>
+                  <Tab style={{ fontSize: 14, fontWeight: 'bold', color: '#532d91' }} label="RECEIVE" value="receive">
                     <div>
-                    <Tabs
-                          value={this.state.value}
-                          onChange={this.handleChange}
-                          tabItemContainerStyle={{
-                            backgroundColor: '#FAFAFA'
-                          }}
-                          inkBarStyle={{backgroundColor: '#532d91'}}
-                          >
-                          <Tab style={{fontSize: 14, fontWeight: 'bold', color: '#532d91'}} label="HISTORY" value="history">
-                            <History local_address={this.state.local_address} />
-                          </Tab>
-                          <Tab style={{fontSize: 14, fontWeight: 'bold', color: '#532d91'}} label="SEND" value="send">
-                            <SendComponent local_address={this.state.local_address} />
-                          </Tab>
-                          <Tab style={{fontSize: 14, fontWeight: 'bold', color: '#532d91'}} label="RECEIVE" value="receive">
-                            <div>
-                             <ReceiveComponent local_address={this.state.local_address} />
-                            </div>
-                          </Tab>
-                        </Tabs>
+                      <ReceiveComponent local_address={this.state.local_address} />
                     </div>
-                </div>
-                  </div>
-                </div>
-            </MuiThemeProvider>
-        );
-    }
+                  </Tab>
+                  <Tab style={{ fontSize: 14, fontWeight: 'bold', color: '#532d91' }} label="VPN List" value="vpn">
+                    <VPNComponent
+                      local_address={this.state.local_address}
+                      status={this.state.status}
+                      vpnData={this.state.vpnData}
+                    />
+                  </Tab>
+                </Tabs>
+              </div>
+            </div>
+          </div>
+        </div>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default Dashboard;

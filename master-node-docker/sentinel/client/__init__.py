38d9aa501667343dde4bacdd0898fe8d47056ad7
Tony Stark <ironman0x7b2@protonmail.com>
2018-01-24 18:51:19 +0530

    Added calculating gas units.

--- master-node-docker/sentinel/client/__init__.py
+++ master-node-docker/sentinel/client/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from .account import CreateNewAccount
 from .account import GetBalance
 from .transactions import TransferAmount
+from .transactions import CalculateGasUnits
 from .transactions import TranscationReceipt
 from .transactions import TransactionHistory
 from .vpn import GetVpnCredentials

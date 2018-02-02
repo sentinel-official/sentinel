3c6a68629c2e043cb2b16a9fb4c868fc4c5e5ea8
fabulous345 <fabulous345@protonmail.com>
2018-02-02 17:22:55 +0530

    Added local server
    Former-commit-id: 5f131e8c22a8e8e058774fb822a35d0ae066a6e1

---
+++ vpn-node-docker/server.py
@@ -0,0 +1,34 @@
+import json
+import falcon
+
+from sentinel.server_node import RegisterNode
+from sentinel.server_node import UpdateNodeInfo
+from sentinel.server_node import DeRegisterNode
+from sentinel.server_node import AddVpnUsage
+from sentinel.server_node import CreateNewAccount
+
+from sentinel.master import GetMasterToken
+from sentinel.client import GenerateOVPN
+from sentinel.utils import JSONTranslator
+
+
+class Up():
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({'status': 'UP'})
+
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.body = json.dumps({'status': 'UP'})
+
+
+app = falcon.API(middleware=[JSONTranslator()])
+app.add_route('/', Up())
+
+# Nodes
+app.add_route('/node', Up())
+app.add_route('/node/account', CreateNewAccount())
+app.add_route('/node/register', RegisterNode())
+app.add_route('/node/update-nodeinfo', UpdateNodeInfo())
+app.add_route('/node/add-usage', AddVpnUsage())
+app.add_route('/node/deregister', DeRegisterNode())

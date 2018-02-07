a222ec0249623b41b8c255cf3ba680eab551b816
fabulous345 <fabulous345@protonmail.com>
2018-02-07 12:58:23 +0530

    Modified docker file changes and ip
    Former-commit-id: c4200c0df8e8d9b125bed98b55181666661301fc

--- master-node-docker/run.sh
+++ master-node-docker/run.sh
@@ -1,21 +1,12 @@
 IP_ADDRESS=$(wget -qO- http://ipecho.net/plain; echo)
-SENTINEL_DIR=$HOME/.ethereum/sentinel;
+SENTINEL_DIR=$HOME/.ethereum;
 
 cd /root;
 
-# if [ "$SENT_ENV" != "DEV" ]; then
-#   geth --datadir "$SENTINEL_DIR" &
-# else
-#   geth --datadir "$SENTINEL_DIR" --rpc --rpcaddr=0.0.0.0 --rpccorsdomain "*" --rpcapi="admin,debug,eth,miner,net,personal,rpc,txpool,web3" &
-# fi
-if [ "$SENT_ENV" != "dev" ]; then
-  geth init genesis.json --datadir "$HOME/.ethereum/sentinel" && \
-  nohup geth --identity "$IP_ADDRESS:masternode" --datadir "$HOME/.ethereum/sentinel" --networkid 87411478 >> /dev/null &
+if [ "$SENT_ENV" != "DEV" ]; then
+  geth --datadir "$SENTINEL_DIR" &
 else
-  geth init genesis.json --datadir "$HOME/.ethereum/sentinel" && \
-  nohup geth --identity "$IP_ADDRESS:masternode" --datadir "$HOME/.ethereum/sentinel" --networkid 87411478 \
-    --fast --rpc --rpcaddr=0.0.0.0 --rpccorsdomain "*" --rpcapi="admin,debug,eth,miner,net,personal,rpc,txpool,web3" \
-    --nodiscover --mine --minerthreads 1 >> /dev/null &
+  geth --rinkeby --datadir "$SENTINEL_DIR" --rpc --rpcaddr=0.0.0.0 --rpccorsdomain "*" --rpcapi="admin,debug,eth,miner,net,personal,rpc,txpool,web3" &
 fi
 
 sleep 5;

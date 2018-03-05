35f0a0a7d16744780ca7a4ab33d7888efe902b84
Tony Stark <ironman0x7b2@protonmail.com>
2018-03-05 11:44:56 +0530

    Changed openvpn shell scripts for multiple sessions
    Former-commit-id: 641442e9603092c08d9ecff6fd53ad174ab656e2

--- vpn-node-docker/sentinel/shell_scripts/init.sh
+++ vpn-node-docker/sentinel/shell_scripts/init.sh
@@ -2,6 +2,9 @@ UFW_RULES=/etc/ufw/before.rules
 UFW_DEFAULT=/etc/default/ufw
 DEFAULT_ROUTE=$(ip route | grep default | awk '{ print $5 }')
 SYSCTL=/etc/sysctl.conf
+KEYS=/etc/openvpn/keys
+ERSA_DIR=/usr/share/easy-rsa
+PUBLIC_IP=$(wget -qO- http://ipecho.net/plain; echo)
 
 cd /root/sentinel/shell_scripts/ && mv server.conf client.conf /etc/openvpn/ && \
 
@@ -16,3 +19,16 @@ sed -i '1s@^@*nat\n@g' $UFW_RULES && \
 sed -i 's@DEFAULT_FORWARD_POLICY@DEFAULT_FORWARD_POLICY="ACCEPT"\n# DEFAULT_FORWARD_POLICY@g' $UFW_DEFAULT && \
 sed -i 's@DEFAULT_INPUT_POLICY@DEFAULT_INPUT_POLICY="ACCEPT"\n# DEFAULT_INPUT_POLICY@g' $UFW_DEFAULT && \
 ufw disable && ufw enable;
+
+# Server keys
+cd $ERSA_DIR && rm -rf pki && \
+./easyrsa init-pki && \
+echo \r | ./easyrsa build-ca nopass && \
+./easyrsa build-server-full server nopass && \
+./easyrsa gen-dh && \
+openvpn --genkey --secret pki/ta.key && \
+
+rm -rf $KEYS && mkdir $KEYS && cd pki && \
+cp *.crt *.key *.pem private/*.key issued/*.crt reqs/*.req $KEYS && \
+
+sed -i -r 's@(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b@'"$PUBLIC_IP"'@g' /etc/openvpn/client.conf;

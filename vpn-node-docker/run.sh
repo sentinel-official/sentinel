e42380579edfa84143080c7a86b97676920cd9f5
Tony Stark <ironman0x7b2@protonmail.com>
2018-03-16 16:16:20 +0530

    Updated run file of VPN
    Former-commit-id: 20cca7ed4eac629d5e3770e82a076af919eb68a2

--- vpn-node-docker/run.sh
+++ vpn-node-docker/run.sh
@@ -3,17 +3,36 @@ if [ -f "$ACCOUNT_DATA" ]; then
     echo "$ACCOUNT_DATA found."
 else
     echo "$ACCOUNT_DATA not found."
-    echo -n "Do you have a wallet address? [yY/nN]: "
-    read option
-    if [ "$option" == "y" ] || [ "$option" == "Y" ]; then
-        echo -n "Please enter your wallet address: "
-        read ADDRESS
-        touch $ACCOUNT_DATA
-        echo '{"keystore": "", "private_key": "", "password": "", "addr": "'${ADDRESS}'", "token": null}' > $ACCOUNT_DATA
-    else
-        echo -n "Please enter password for creating new wallet: "
-        read PASSWORD
-    fi
+    OK=NOTOK
+    while [ "$OK" == "NOTOK" ]; do
+        echo -n "Do you have a wallet address? [Y/N]: "
+        read option
+        if [ "$option" == "y" ] || [ "$option" == "Y" ]; then
+            echo -n "Please enter your wallet address: "
+            read ADDRESS
+            if [ ${#ADDRESS} -ne 42 ]; then
+                echo 'Wrong wallet address length.'
+                continue
+            else
+                touch $ACCOUNT_DATA
+                echo '{"keystore": "", "private_key": "", "password": "", "addr": "'${ADDRESS}'", "token": null}' > $ACCOUNT_DATA
+            fi
+        else
+            echo -n "Please enter password for creating new wallet: "
+            read PASSWORD
+            if [ ${#PASSWORD} -le 0 ]; then
+                echo "Password length must be greater than zero."
+                continue
+            fi
+        fi
+        echo -n "Is everything correct ? [Y/N]: "
+        read option
+        if [ "$option" == "y" ] || [ "$option" == "Y" ]; then
+            OK=OK
+        else
+            OK=NOTOK
+        fi
+    done
 fi
 
 cd /root;

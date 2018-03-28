b45ea19d9beff447eeec2bc86d9ee8fc6af54ed3
fabulous345 <fabulous345@protonmail.com>
2018-03-28 18:48:22 +0530

    Added vpn list with search option
    Former-commit-id: c6a01186c12fdef9c1400a957089e489d1bcb339

--- wallet/src/Components/VPNHistory.js
+++ wallet/src/Components/VPNHistory.js
@@ -141,7 +141,7 @@ class VPNHistory extends Component {
                                 <span style={{ fontWeight: 600}}>Time: </span>{new Date(sessionData.timestamp * 1000).toGMTString()}
                             </CardText>
                             {
-                                sessionData.is_paid ?
+                                sessionData.is_payed ?
                                     <span>
                                         <Done style={{ float: 'right', marginTop: '-7%', marginRight: '1%' }}
                                             data-tip data-for="payed" color="green" />

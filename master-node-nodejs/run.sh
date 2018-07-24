206a63a755cae8442a5fcd6edc147f7f473f48e4
Thanos Titan <31741756+than-os@users.noreply.github.com>
2018-07-24 12:49:17 +0530

    Update run.sh

--- master-node-nodejs/run.sh
+++ master-node-nodejs/run.sh
@@ -1,5 +1,5 @@
 nohup mongod >> /dev/null &
 nohup mongo >> /dev/null &
 nohup redis-server >> /dev/null &
-cd /root/sentinel/src && npm install && npm run build
+cd /root/sentinel/src && npm install
 npm run dev

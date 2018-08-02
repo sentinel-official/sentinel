5c0d6bbf0fc6d299402c888dce149ee870aefca0
williams <williamjennings>
2018-08-02 15:03:45 +0530

    swap schema changed

--- master-node-nodejs/src/models/swaps.js
+++ master-node-nodejs/src/models/swaps.js
@@ -7,8 +7,12 @@ let swapSchema = new Schema({
   to_symbol: String,
   from_address: String,
   to_address: String,
+  tx_hash_0:String,
   time_0: Number,
-  status: Number
+  status: Number,
+  message:String,
+  tx_hash_1:String,
+  time_1:Number
 });
 
 export const Swap = mongoose.model('Swap', swapSchema);

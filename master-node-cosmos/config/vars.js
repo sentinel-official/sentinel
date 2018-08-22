1a367ce6fcd4aa34a0542df6b16d65d25674886a
Tony Stark <ironman0x7b2@protonmail.com>
2018-08-22 17:04:52 +0530

    Added basic config files

---
+++ master-node-cosmos/config/vars.js
@@ -0,0 +1,16 @@
+let server = {
+  port: 8000
+};
+
+let mongoDb = {
+  address: '127.0.0.1',
+  port: 27017,
+  username: '',
+  password: '',
+  dbName: 'sentinel'
+};
+
+module.exports = {
+  server,
+  mongoDb
+};

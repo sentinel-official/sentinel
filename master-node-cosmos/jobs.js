3f583c6af3133263e5799c21303dfaae8f7536a3
Tony Stark <ironman0x7b2@protonmail.com>
2019-01-04 16:17:37 +0530

    Updated jobs

--- master-node-cosmos/jobs.js
+++ master-node-cosmos/jobs.js
@@ -1,61 +1,50 @@
 let async = require('async');
-let lodash = require('lodash');
 let nodeDbo = require('./server/dbos/node.dbo');
 let sessionDbo = require('./server/dbos/session.dbo');
 
 let MAX_SECS = 2 * 60 * 1000;
 
-let endDeadSessions = (deadTime, cb) => {
-  async.waterfall([
-    (next) => {
-      nodeDbo.getNodes({
-        'status': 'down',
-        'statusOn': deadTime
-      }, (error, result) => {
-        if (error) next(error);
-        else {
-          let addresses = lodash.map(result, 'accountAddress');
-          next(null, addresses);
-        }
-      });
-    }, (addresses, next) => {
+let endSessions = (nodes, cb) => {
+  async.each(nodes,
+    (node, next) => {
       sessionDbo.updateSessions({
-        'nodeAccountAddress': {
-          $in: addresses
-        },
-        'startedOn': {
-          $exists: true
-        },
+        'nodeAccountAddress': node.accountAddress,
         'endedOn': {
           $exists: false
         }
       }, {
-          'endedOn': now
-        }, next);
+          'endedOn': node.statusOn
+        }, (error, result) => next(null));
+    }, cb);
+};
+
+let sessionDeadThread = () => {
+  async.waterfall([
+    (next) => {
+      nodeDbo.getNodes({
+        'status': 'down'
+      }, (error, result) => {
+        if (error) next(error);
+        else endSessions(result, next);
+      });
     }
-  ], cb);
+  ], (error, result) => { });
 };
 
 let nodeDeadThread = () => {
   let now = new Date();
   let minTime = new Date(now - MAX_SECS);
-  async.waterfall([
-    (next) => {
-      nodeDbo.updateNodes({
-        'statusOn': {
-          $lt: minTime
-        }
-      }, {
-          'status': 'down',
-          'statusOn': now
-        }, (error, result) => {
-          if (error) next(error);
-          else if (result.modifiedCount > 0) endDeadSessions(now, next);
-          else next(null);
-        });
+  nodeDbo.updateNodes({
+    'statusOn': {
+      $lt: minTime
     }
-  ], (error, result) => { });
+  }, {
+      'status': 'down',
+      'statusOn': now
+    }, (error, result) => { });
 };
 
 nodeDeadThread();
+sessionDeadThread();
 setInterval(nodeDeadThread, MAX_SECS / 2);
+setInterval(sessionDeadThread, MAX_SECS * 2.5);
